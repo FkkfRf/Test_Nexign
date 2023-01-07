638f2648b4ab7dacae7ac68241bcd4dc38aa24f1
FkkfRf <Fkkf.Rf@gmail.com>
2023-01-08 01:41:13 +0300

    ControlElements

--- src/test/java/page/MainPage.java
+++ src/test/java/page/MainPage.java
@@ -4,20 +4,26 @@ import com.codeborne.selenide.SelenideElement;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Configuration.baseUrl;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class MainPage {
-    private final static String WELCOME_BANNER_TEXT = "Безграничные возможности монетизации традиционных и новых цифровых услуг ";
+
     private final static SelenideElement
             welcomeBannerText = $(".welcome-banner__content-subtitle"),
             searchInHeaderControls = $(".header__search "),
             searchTextInput = $("[id=edit-search-api-fulltext]"),
             searchLink = $(".line-group-search"),
-            searchTextCheck = $(".page-content");
+            searchTextCheck = $(".page-content"),
+            languageSet = $(".language");
 
     public MainPage openPage() {
         open(baseUrl);
-        welcomeBannerText.shouldHave(text(WELCOME_BANNER_TEXT));
+        return this;
+    }
+
+    public MainPage checkWelcomeBannerText(String welcomeBannerTextSet) {
+        welcomeBannerText.shouldHave(text(welcomeBannerTextSet));
         return this;
     }
 
@@ -38,6 +44,12 @@ public class MainPage {
 
     public MainPage searchTextCheck(String searchText) {
         searchTextCheck.shouldHave(text(searchText));
+        return this;
+    }
+
+    public MainPage selectLanguageInHeaderControls(String selectLanguage) {
+        languageSet.click();
+        $(byText(selectLanguage)).click();
         return this;
     }
 }
---
+++ src/test/java/tests/HeaderControlsTest.java
@@ -0,0 +1,69 @@
+package tests;
+
+import base.BaseTest;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Link;
+import io.qameta.allure.Owner;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import page.MainPage;
+
+import static io.qameta.allure.Allure.step;
+import static java.lang.Thread.sleep;
+
+public class HeaderControlsTest extends BaseTest {
+    private final static String WELCOME_BANNER_TEXT_RUS = "Безграничные возможности монетизации традиционных и новых цифровых услуг";
+    private final static String WELCOME_BANNER_TEXT_ENG = "Advanced monetization on a single convergent platform";
+    private final static String searchText = "мегафон";
+    MainPage MainPage = new MainPage();
+
+    @Feature("Проверка панели элементов")
+    @DisplayName("Проверка поиска по сайту")
+    @Owner("FkkfRf")
+    @Link(url = "https://nexign.com/ru")
+    @Test
+    void searchFromSiteTest() {
+        step("Открываем сайт", () -> {
+            MainPage.openPage();
+            MainPage.checkWelcomeBannerText(WELCOME_BANNER_TEXT_RUS);
+        });
+        step("Кликаем по иконке поиска ", () -> {
+            MainPage.clickSearchInHeaderControls();
+        });
+        step("Вводим текст в поле поиска", () -> {
+            MainPage.searchTextSetValue(searchText);
+        });
+        step("Переходим по первой ссылке", () -> {
+            MainPage.openSearchLink();
+        });
+        step("Проеряем, что такой текст есть на странице", () -> {
+            MainPage.searchTextCheck(searchText);
+        });
+    }
+
+
+    @Feature("Проверка панели элементов")
+    @DisplayName("Проверка ереключения языка")
+    @Owner("FkkfRf")
+    @Link(url = "https://nexign.com/ru")
+    @Test
+    void selectLanguageTest() {
+        step("Открываем сайт", () -> {
+            MainPage.openPage();
+        });
+        step("Выбираем язык сайта ENG ", () -> {
+            MainPage.selectLanguageInHeaderControls("ENG");
+        });
+        step("Проверям, что язык установлен  ENG ", () -> {
+            MainPage.checkWelcomeBannerText(WELCOME_BANNER_TEXT_ENG);
+        });
+        step("Выбираем язык сайта RUS ", () -> {
+            MainPage.selectLanguageInHeaderControls("RUS");
+        });
+        step("Проверям, что язык установлен  RUS ", () -> {
+            sleep(3000);
+            MainPage.checkWelcomeBannerText(WELCOME_BANNER_TEXT_RUS);
+        });
+    }
+}
+
