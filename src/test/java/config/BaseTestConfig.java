b60a02fe1c5b9eb95f0fa1bc5d9f3c0b9e8a237d
FkkfRf <Fkkf.Rf@gmail.com>
2023-01-11 19:06:51 +0300

    Config

--- src/test/java/config/BaseTestConfig.java
+++ src/test/java/config/BaseTestConfig.java
@@ -3,7 +3,7 @@ package config;
 import org.aeonbits.owner.Config;
 
 //@Config.Sources({
-//"classpath:resources/config/${env}.properties"
+//"classpath: config/${env}.properties"
 //})
 public interface BaseTestConfig extends Config {
     @Key("baseUrl")
