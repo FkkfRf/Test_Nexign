6b8bd9da891d130574648879e769857c58b1a92a
FkkfRf <Fkkf.Rf@gmail.com>
2023-01-11 18:47:15 +0300

    Config

--- src/test/java/config/ProjectProvider.java
+++ src/test/java/config/ProjectProvider.java
@@ -5,14 +5,13 @@ import org.aeonbits.owner.ConfigFactory;
 
 public class ProjectProvider {
     static BaseTestConfig configBase = ConfigFactory.create(BaseTestConfig.class, System.getProperties());
-    static String remoteUrl = System.getProperty("remote", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
     public static void configBase() {
         Configuration.baseUrl = ProjectProvider.configBase.getBaseUrl();
         Configuration.browser = ProjectProvider.configBase.getBrowser();
         Configuration.browserVersion = ProjectProvider.configBase.getBrowserVersion();
         Configuration.browserSize = ProjectProvider.configBase.getBrowserSize();
         Configuration.remote = ProjectProvider.configBase.getRemoteURL();
-        //String remoteUrl = ProjectProvider.configBase.getRemoteURL();
+        String remoteUrl = ProjectProvider.configBase.getRemoteURL();
         if (remoteUrl != null) {
             Configuration.remote = remoteUrl;
         }
